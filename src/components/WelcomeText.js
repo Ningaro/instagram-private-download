84dc220af4ba7b2416b2294b69b3b893dc2b22cb
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-01-30 02:26:35 +0300

    🔖Version 2.0.0
    ✨Full migrate to React
    👽Update links to download content
    ⚰️Remove links to bot

---
+++ src/components/WelcomeText.js
@@ -0,0 +1,67 @@
+// UI elems
+import {
+    Container,
+    OrderedList,
+    ListItem,
+    Text,
+    Divider,
+    Stack,
+    Tooltip,
+    Button,
+    Center,
+    chakra,
+    useColorMode
+} from "@chakra-ui/react"
+
+/**
+ * First page with info about site
+ * @returns {React}
+ */
+export default function WelcomeText({ changeStep }) {
+    const { colorMode } = useColorMode()
+
+    return (
+        <Container h="100%">
+            <Center h="100%">
+                <Stack spacing={8}>
+                    <Text>
+                        На этом сайте вы можете бесплатно и <SaveTip><chakra.span as="u" color="blue.300">безопасно</chakra.span></SaveTip> скачать любую историю из социальной сети Instagram.
+                        Фактически данный сайт является простым инстурментом, который выполняет ряд функций за вас.
+                    </Text>
+                    <Divider />
+                    <Text>
+                        Вот, что делает сайт наглядно:
+                    </Text>
+                    <Container>
+                        <OrderedList>
+                            <ListItem>Совершает запрос на сервер Instagram от вашего имени</ListItem>
+                            <ListItem>Получает данные о всех последних историях конкретного аккаунта</ListItem>
+                            <ListItem>Представляет полученную информацию в удобном виде</ListItem>
+                        </OrderedList>
+                    </Container>
+                    <Center>
+                        <Button
+                            colorScheme={colorMode === 'dark' ? 'cyan' : 'teal'}
+                            onClick={() => changeStep(1)}
+                        >
+                            Хорошо, давайте попробуем!
+                        </Button>
+                    </Center>
+                </Stack>
+
+            </Center>
+        </Container>
+
+    )
+}
+
+function SaveTip(props) {
+    return (
+        <Tooltip
+            label="Мы не храним информацию о наших пользователях, а все функции выполняются локально на вашем устройстве"
+            {...props}
+        >
+            {props.children}
+        </Tooltip>
+    )
+}
