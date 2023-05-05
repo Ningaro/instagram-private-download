e36abf65091bbc887038f302b91a973fd12e6d80
Ningaro <23503775+Ningaro@users.noreply.github.com>
2023-05-05 22:47:08 +0300

    🐛 Fix connect data

--- index.js
+++ index.js
@@ -10,32 +10,39 @@ app.use(express.json())
 app.post('/getUserStories', async (req, response) => {
     if (req.body.login && req.body.token) {
         try {
+            axios.defaults.headers['Cookie'] = req.body.token
+            axios.defaults.headers['x-ig-app-id'] = '936619743392459'
+
             const res1 = await axios.get(
                 `https://www.instagram.com/stories/${req.body.login}/`
             )
 
             const html = res1.data
-            const posStart = html.indexOf('{"StoriesPage":')
+            const searchStartStr = '"id":"'
+            const searchEndStr = '","'
+            const posStart = html.indexOf(searchStartStr)
             const posEnd =
-                html.substring(posStart).indexOf('}}]}') + posStart + 4
+                html.substring(posStart).indexOf(searchEndStr) + posStart
 
-            console.warn(html.substring(posStart, posEnd))
-
-            const userID = JSON.parse(html.substring(posStart, posEnd))
-                .StoriesPage[0].user.id
-
-            axios.defaults.headers['Cookie'] = req.body.token
-            axios.defaults.headers['x-ig-app-id'] = '152431142231154'
+            const userID = html.substring(
+                posStart + searchStartStr.length,
+                posEnd
+            )
 
             const res2 = await axios.get(
-                `https://i.instagram.com/api/v1/feed/reels_media/?reel_ids=${userID}`
+                `https://www.instagram.com/api/v1/feed/reels_media/?reel_ids=${userID}`
+            )
+
+            console.log(
+                `https://www.instagram.com/api/v1/feed/reels_media/?reel_ids=${userID}`,
+                res2.data
             )
 
             if (typeof res2.data !== 'object')
                 response.status(412).send('412 Precondition Failed')
             else response.send(res2.data)
         } catch (e) {
-            console.error(e)
+            console.error(e.message)
             response.status(500).send('500 Internal Server Error')
         }
     } else response.status(412).send('412 Precondition Failed')
